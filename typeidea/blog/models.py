9748b36867efd68e0ddbd09faa24db0cb14b8981
qxh <2498884969@qq.com>
2019-04-21 21:14:33 +0800

    manytomany

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -14,6 +14,9 @@ class Category(models.Model):
     owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = verbose_name_plural = '分类'
 
@@ -23,8 +26,12 @@ class Tag(models.Model):
     STATUS_DELETE = 0
     STATUS_ITEMS = ((STATUS_NORMAL, '正常'), (STATUS_DELETE, '删除'))
     name = models.CharField(max_length=10, verbose_name='名称')
-    status = models.ForeignKey(User, verbose_name='作者')
+    status = models.PositiveIntegerField(choices=STATUS_ITEMS, default=1, verbose_name='状态')
+    owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = verbose_name_plural = '标签'
@@ -38,12 +45,15 @@ class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name='标题')
     desc = models.CharField(max_length=1024, blank=True, verbose_name='摘要')
     content = models.TextField(verbose_name='正文', help_text='正文必须为Markdown格式')
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')
+    category = models.ForeignKey(Category, default=None, verbose_name='分类')
     tag = models.ManyToManyField(Tag, verbose_name='标签')
     owner = models.ForeignKey(User, verbose_name='作者')
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    def __str__(self):
+        return self.title
+
     class Meta:
-        verbose_name = verbose_name_plural = '标签'
+        verbose_name = verbose_name_plural = '文章'
         ordering = ['-id']  # 根据id进行降序排序
-
-
--- typeidea/typeidea/settings/base.py
+++ typeidea/typeidea/settings/base.py
@@ -122,3 +122,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# 日志
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+    }
+}
