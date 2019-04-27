bf9961f11ec2f2e3fa209393f49ed830c3e49c84
qxh <2498884969@qq.com>
2019-04-27 17:23:44 +0800

    初探views

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -15,9 +15,26 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+
 from .custom_site import custom_site
+from blog.views import post_list, post_detail
+from config.views import links
+
+"""
+1. 博客首页：/
+2. 博文详情页:/post/<post_id>.html
+3. 分类列表页：/category/<category_id>/
+4. 标签列表页：/tag/<tag_id>/
+5. 友链展示页：/links/
+"""
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^super_admin/', custom_site.urls)
+    url(r'^super_admin/', custom_site.urls),
+    url(r'^$', post_list),
+    url(r'^category/(?P<category_id>\d+)/$', post_list, name='category_list'),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail),
+    url(r'^links/$', links)
+
 ]
